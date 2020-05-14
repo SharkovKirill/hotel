05c7ef9b763a2404244b5cb1cc66263f2f8e6061
Vladimir Ermolenko <v.ermolenko@g.nsu.ru>
2020-05-15 01:46:20 +0700

    practically nothing

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 class Client:
     def __init__(self, line):
-        self.reserve = line[0]
+        self.book_date = line[0]
         self.surname = line[1]
         self.name = line[2]
         self.patr = line[3]
-        self.numper = line[4]
+        self.num_per = line[4]
         self.arrival = line[5]
-        self.numdate = line[6]
-        self.max_sum = line[7]
+        self.num_days = line[6]
+        self.max_price = line[7]
 
     def __str__(self):
         return self.name  # вместо name можно ставить, что нужно
@@ -17,11 +17,16 @@ class Client:
 
 
 class Room:
+
+    degree_of_comfort = {"стандарт": 1, "стандарт_улучшенный": 1.2, "апартамент": 1.5}
+    room_type = {'одноместный': 2900, 'двухместный': 2300, 'полулюкс': 3200, 'люкс': 4100}
+
     def __init__(self, line):
         self.id = line[0]
         self.type = line[1]
         self.number_of_persons = line[2]
         self.comfort = line[3]
+        self.price = Room.room_type[self.type] * Room.degree_of_comfort[self.comfort]
 
     def __str__(self):
         return self.id  # вместо id можно ставить, что нужно
@@ -30,14 +35,7 @@ class Room:
         return self.__str__()
 
 
-comfort_factor = [1, 1.2, 1.5]
-degree_of_comfort = {1: "стандарт", 1.2: "стандарт_улучшенный", 1.5: "апартамент"}
-
-room_type = {'одноместный': 2900, 'Двухместный': 2300, 'Полулюкс': 3200, 'Люкс': 4100}
-
-food_factor = [0, 280, 1000]
 food_cost = {0: 'без питания', 280: 'завтрак', 1000: 'полупансион'}
-
 
 
 clients = []
@@ -45,16 +43,27 @@ with open('booking.txt', 'r', encoding='utf-8') as booking:
     for line in booking.readlines():
         client = line.split()
         clients.append(Client(client))
-print(clients[1].__dict__)
+# print(clients[1].__dict__)
+
 
 rooms = []
 with open('fund.txt', 'r', encoding='utf-8') as fund:
     for line in fund.readlines():
         room = line.split()
         rooms.append(Room(room))
-print(rooms[15].__dict__)  # пример как можно доставать инфу в виде словаря
+# print(rooms[15].__dict__)  # пример как можно доставать инфу в виде словаря
+
+
+# начало алгоритма подбора
+
+for guest in clients:
+    for room_ in rooms:
+
+
+
+
 '''
 Kirill - vvod
-Vova,Nikita - proverka
+Vova, Nikita - proverka
 Sveta - print, PEP8
 '''
