571782f0086730f37ddc4e1fa7537d2a5ce923f6
SharkovKirill <kirill.office75@gmail.com>
2020-05-14 14:52:42 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+class Client:
+    def __init__(self, line):
+        self.reserve = line[0]
+        self.surname = line[1]
+        self.name = line[2]
+        self.patr = line[3]
+        self.numper = line[4]
+        self.arrival = line[5]
+        self.numdate = line[6]
+        self.max_sum = line[7]
+    def __str__(self):
+        return self.surname
+    def __repr__(self):
+        return self.__str__()
+
+#degree_of_comfort = {"стандарт" : 1, "стандарт_улучшенный" : 1.2, "апартамент" : 1.5}
+
+clients = []
+with open('booking.txt', 'r', encoding='utf-8') as booking:
+    for line in booking.readlines():
+        client = line.split()
+        clients.append(Client(client))
+print(degree_of_comfort['апартамент'])
