244afef75c0e7ad5f46db3c6b406e868b60eb35c
nikitadracev <npcweb@mai.ru>
2020-05-16 13:27:27 +0700

    Add documentation string and comments

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import random
 
 
 class Client:
+    '''Customer class'''
+
     def __init__(self, line):
+        '''Initialization method'''
+
         self.book_date = line[0]
         self.surname = line[1]
         self.name = line[2]
@@ -12,6 +16,7 @@ class Client:
         self.num_days = datetime.timedelta(days=int(line[6]))
         self.max_price = int(line[7])
 
+        #Str into datetime
         arrival = line[5].split('.')[::-1]
         for dt in range(len(arrival)):
             arrival[dt] = int(arrival[dt])
@@ -20,18 +25,25 @@ class Client:
         self.arrival = arrival
 
     def __str__(self):
+        '''String representation method'''
+
         return self.name  # вместо name можно ставить, что нужно
 
     def __repr__(self):
+        '''Representation method'''
+
         return self.__str__()
 
 
 class Room:
+    '''Room class'''
 
     degree_of_comfort = {'стандарт': 1, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
     room_type = {'одноместный': 2900, 'двухместный': 2300, 'полулюкс': 3200, 'люкс': 4100}
 
     def __init__(self, line):
+        '''Initialization method'''
+
         self.id = line[0]
         self.type = line[1]
         self.number_of_persons = line[2]
@@ -41,9 +53,13 @@ class Room:
         self.food = 'без питания'
 
     def __str__(self):
+        '''String representation method'''
+
         return self.id  # вместо id можно ставить, что нужно
 
     def __repr__(self):
+        '''Representation method'''
+
         return self.__str__()
 
     def __lt__(self, other):
@@ -67,14 +83,14 @@ class Room:
 
 food_cost = {'без питания': 0, 'завтрак': 280, 'полупансион': 1000}
 
-
+#Open file with guests and send them to the list
 clients = []
 with open('booking.txt', 'r', encoding='utf-8') as booking:
     for line_p in booking.readlines():
         client = line_p.split()
         clients.append(Client(client))
 
-
+#Open file with rooms and send them to the list
 rooms = []
 with open('fund.txt', 'r', encoding='utf-8') as fund:
     for line_n in fund.readlines():
@@ -84,14 +100,17 @@ with open('fund.txt', 'r', encoding='utf-8') as fund:
 
 f_data = dict()
 
+#Full scan
 for guest in clients:
     filtered = []
+    #Taking rooms
     for p_room in rooms:
         if not p_room.occupied or p_room.occupied <= guest.arrival:
             if p_room.number_of_persons == guest.num_per:
                 if p_room.price <= guest.max_price:
                     filtered.append(p_room)
 
+    #Taking rooms with discount 30%
     if len(filtered) == 0:
         for p_room in rooms:
             if not p_room.occupied or p_room.occupied <= guest.arrival:
@@ -99,6 +118,7 @@ for guest in clients:
                     if p_room.price * 0.7 <= guest.max_price:
                         filtered.append(p_room)
 
+        #Add food price to the final cost (for dicount)
         filtered.sort(reverse=True)
         for rm in filtered:
             if guest.max_price - rm.price * 0.7 >= 1000:
@@ -108,6 +128,7 @@ for guest in clients:
                 rm.price += 280
                 rm.food = 'завтрак'
 
+    # Add food price to the final cost
     filtered.sort(reverse=True)
     for rm in filtered:
         if guest.max_price - rm.price >= 1000:
@@ -117,6 +138,7 @@ for guest in clients:
             rm.price += 280
             rm.food = 'завтрак'
 
+    #The opportunity to cancel the room
     if len(filtered) != 0:
         filtered.sort(reverse=True)
         if random.random() >= 0.25:
