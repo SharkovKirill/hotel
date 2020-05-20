35c276ec8d489d2e5e5a4541d0748c1dcaff143a
Vladimir Ermolenko <v.ermolenko@g.nsu.ru>
2020-05-20 23:23:00 +0700

    all_done

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-'''
+"""
 Developers: Sharkov Kirill -  25%
             Nikita Drachev - 25%
             Vladimir Ermolenko - 25%
             Svetlana Keda - 25%
-'''
+"""
+
 import datetime
 import random
 
 
 class Client:
-    '''Customer class'''
+    """Customer class"""
 
     def __init__(self, line):
-        '''Initialization method'''
-
+        """Initialization method"""
         self.book_date = line[0]
         self.surname = line[1]
         self.name = line[2]
         self.patr = line[3]
         self.fullname = line[1] + ' ' + line[2] + ' ' + line[3]
-        self.num_per = line[4]
+        self.num_per = int(line[4])
         self.num_days = datetime.timedelta(days=int(line[6]))
-        self.max_price = int(line[7])
+        self.max_price = int(line[7]) * int(self.num_per)
+        self.max_price_night = int(line[7])
         self.ar = line[5]
         self.num_d = line[6]
 
@@ -34,41 +35,37 @@ class Client:
         self.arrival = arrival
 
     def __str__(self):
-        '''String representation method'''
-
+        """String representation method"""
         return self.name
 
     def __repr__(self):
-        '''Representation method'''
-
+        """Representation method"""
         return self.__str__()
 
 
 class Room:
-    '''Room class'''
-
+    """Room class"""
     degree_of_comfort = {'стандарт': 1, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
     room_type = {'одноместный': 2900, 'двухместный': 2300, 'полулюкс': 3200, 'люкс': 4100}
 
     def __init__(self, line):
-        '''Initialization method'''
-
+        """Initialization method"""
         self.id = line[0]
         self.type = line[1]
-        self.number_of_persons = line[2]
+        self.number_of_persons = int(line[2])
         self.comfort = line[3]
-        self.price = Room.room_type[self.type] * Room.degree_of_comfort[self.comfort]
+        self.price = int(Room.room_type[self.type] * Room.degree_of_comfort[self.comfort] * self.number_of_persons)
+        self.price_d = int(self.price * 0.7)
         self.occupied = False
         self.food = 'без питания'
+        self.disc = False
 
     def __str__(self):
-        '''String representation method'''
-
+        """String representation method"""
         return self.id
 
     def __repr__(self):
-        '''Representation method'''
-
+        """Representation method"""
         return self.__str__()
 
     def __lt__(self, other):
@@ -111,6 +108,7 @@ f_data = dict()
 # Full scan
 for guest in clients:
     filtered = []
+
     # Taking rooms
     for p_room in rooms:
         if not p_room.occupied or p_room.occupied <= guest.arrival:
@@ -118,67 +116,47 @@ for guest in clients:
                 if p_room.price <= guest.max_price:
                     filtered.append(p_room)
 
-    # Taking rooms with discount 30%
     if len(filtered) == 0:
         for p_room in rooms:
             if not p_room.occupied or p_room.occupied <= guest.arrival:
-                if p_room.number_of_persons <= guest.num_per:
-                    if p_room.price * 0.7 <= guest.max_price:
+                if p_room.number_of_persons > guest.num_per:
+                    if p_room.price_d <= guest.max_price:
+                        p_room.disc = True
                         filtered.append(p_room)
 
-        # Add food price to the final cost (for dicount)
-        filtered.sort(reverse=True)
-        for rm in filtered:
-            if guest.max_price - rm.price * 0.7 >= 1000:
-                rm.price += 1000
-                rm.food = 'полупансион'
-            elif guest.max_price - rm.price * 0.7 >= 280:
-                rm.price += 280
-                rm.food = 'завтрак'
-
-    # Add food price to the final cost
-    filtered.sort(reverse=True)
-    for rm in filtered:
-        if guest.max_price - rm.price >= 1000:
-            rm.price += 1000
-            rm.food = 'полупансион'
-        elif guest.max_price - rm.price >= 280:
-            rm.price += 280
-            rm.food = 'завтрак'
-
-    # The opportunity to cancel the room
-    profit = 0
     if len(filtered) != 0:
         filtered.sort(reverse=True)
-        if random.random() >= 0.25:
-            rm_id = filtered[0].id
-            rm_food = filtered[0].food
+        f_rm = filtered[0]
+        if not f_rm.disc:
+            if guest.max_price - f_rm.price >= 1000 * guest.num_per:
+                f_rm.price += 1000 * guest.num_per
+                f_rm.food = 'полупансион'
+            elif guest.max_price - f_rm.price >= 280 * guest.num_per:
+                f_rm.price += 280 * guest.num_per
+                f_rm.food = 'завтрак'
+        else:
+            if guest.max_price - f_rm.price_d >= 1000 * guest.num_per:
+                f_rm.price = f_rm.price_d + 1000 * guest.num_per
+                f_rm.food = 'полупансион'
+            elif guest.max_price - f_rm.price_d >= 280 * guest.num_per:
+                f_rm.price = f_rm.price_d + 280 * guest.num_per
+                f_rm.food = 'завтрак'
 
-            for s_rm in rooms:
-                if s_rm.id == rm_id:
-                    s_rm.occupied = guest.arrival + guest.num_days
+        if random.random() >= 0.25:
+            for rm in rooms:
+                if rm.id == f_rm.id:
+                    rm.occupied = guest.arrival + guest.num_days
                     break
-            total_cost = rm.room_type[rm.type] + food_cost[rm_food]
-            lst = [rm_id, rm_food, total_cost]
-            f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = lst
+
+            opt_lst = [f_rm.id, f_rm.food, f_rm.price]
+            f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = opt_lst
 
         else:
-            rm_id = filtered[0].id
-            rm_food = filtered[0].food
-
-            for s_rm in rooms:
-                if s_rm.id == rm_id:
-                    s_rm.occupied = guest.arrival + guest.num_days
-                    break
-            total_cost = rm.room_type[rm.type] + food_cost[rm_food]
-            lst = ['о', rm_id, rm_food, total_cost]
-            f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = lst  # отказ
-            continue
-
+            opt_lst = ['о', f_rm.id, f_rm.food, f_rm.price]
+            f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = opt_lst
 
     else:
-        f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = 'не найдено'
-        continue
+        f_data[guest.surname + ' ' + guest.name + ' ' + guest.patr] = 'н'
 
 for key in list(f_data.keys()):
     if f_data[key][0] != 'о' and f_data[key][0] != 'н':
@@ -188,19 +166,19 @@ for key in list(f_data.keys()):
                     print('-' * 100)
                     print('Поступила заявка на бронирование: ')
                     print(f'{guest.book_date} {guest.fullname} {guest.num_per} {guest.ar} {guest.num_d} '
-                          f'{guest.max_price}')
+                          f'{guest.max_price_night}')
                     print('Найден: ')
                     print('номер', room.id, room.type, room.comfort, 'рассчитан на', room.number_of_persons, 'чел.',
                           'фактически', guest.num_per, 'чел.', f_data[key][1], 'стоимость', f_data[key][2])
                     print('Клиент согласен. Номер забронирован.')
 
-    elif f_data[key][0] == 'н':
+    elif f_data[key] == 'н':
         for guest in clients:
             if guest.fullname == key:
                 print('-' * 100)
                 print('Поступила заявка на бронирование: ')
                 print(f'{guest.book_date} {guest.fullname} {guest.num_per} {guest.ar} {guest.num_d} '
-                      f'{guest.max_price}')
+                      f'{guest.max_price_night}')
                 print('Предложений по данному запросу нет. В бронировании отказано.')
 
     elif f_data[key][0] == 'о':
@@ -210,7 +188,7 @@ for key in list(f_data.keys()):
                     print('-' * 100)
                     print('Поступила заявка на бронирование: ')
                     print(f'{guest.book_date} {guest.fullname} {guest.num_per} {guest.ar} {guest.num_d} '
-                          f'{guest.max_price}')
+                          f'{guest.max_price_night}')
                     print('Найден: ')
                     print('номер', room.id, room.type, room.comfort, 'рассчитан на', room.number_of_persons, 'чел.',
                           'фактически', guest.num_per, 'чел.', f_data[key][2], 'стоимость', f_data[key][3])
